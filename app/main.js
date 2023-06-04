5a37cf0b8fec29212ee8ec9e3c36e744a2d27815
Rudson_A <abraaorudson1@gmail.com>
2023-06-04 15:19:31 -0300

    mudando para notebook

---
+++ app/main.js
@@ -0,0 +1,6 @@
+const codigo = document.getElementById('codigo');
+const btnHighlight = document.getElementById('btnHighlight');
+const codigoComHighlight = document.getElementById('codigoComHighlight');
+const cor = document.getElementById("corDeFundo")
+console.log(codigoComHighlight.innerHTML);
+console.log(cor.value);

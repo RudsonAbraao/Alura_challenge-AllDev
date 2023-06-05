2e822caa4584065b23d8d078c19d8b95d044a74f
jaqueline <jaquelinenepomuceno.2019@gmail.com>
2023-06-05 00:28:41 -0300

    mudando para pc

--- app/main.js
+++ app/main.js
@@ -1,6 +1,16 @@
-const codigo = document.getElementById('codigo');
-const btnHighlight = document.getElementById('btnHighlight');
-const codigoComHighlight = document.getElementById('codigoComHighlight');
-const cor = document.getElementById("corDeFundo")
-console.log(codigoComHighlight.innerHTML);
-console.log(cor.value);
+const linguagem = document.querySelector('.personalizacao__linguagens');
+const areaDoCodigo =  document.querySelector('.codigo');
+const botao = document.querySelector('#btnHighlight');
+console.log(areaDoCodigo.innerText);
+console.log(areaDoCodigo.innerHTML);
+
+function aplicaHighlight () {
+    const codigo = areaDoCodigo.innerText;
+    areaDoCodigo.innerHTML = `<code class = "preview hljs ${linguagem.value}" contenteditable="true" aria-label= "Editor de código"></code> `;
+    areaDoCodigo.querySelector('code').textContent = codigo;
+    hljs.highlightElement(areaDoCodigo.querySelector('code'))
+};
+
+botao.addEventListener('click', ()=> {
+    aplicaHighlight()
+})

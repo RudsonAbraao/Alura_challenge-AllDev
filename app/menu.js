07f0697d1a912c73da6b69a01f7da7af1f610568
Rudson_A <abraaorudson1@gmail.com>
2023-06-14 18:30:32 -0300

    mudadndo para notebook

---
+++ app/menu.js
@@ -0,0 +1,7 @@
+const btnMenu = document.querySelector('.icone__menu');
+const menu = document.querySelector('.menu');
+btnMenu.addEventListener('click', ()=>{
+    btnMenu.classList.toggle('menu__aberto')
+    console.log('botao menu');
+    menu.classList.toggle('ativo')
+})
